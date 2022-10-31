1902ecaec48565d6e6656ae6ff9e8d97459ca39a
lucascomaru <lucascomaru20@gmail.com>
2022-10-31 10:01:41 -0300

    Formulário construído

--- forms.py
+++ forms.py
@@ -1,16 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class FormCriarConta(FlaskForm):
-    username = StringField('Nome de usuário')
-    email = StringField('E-mail')
-    senha = PasswordField('Senha')
-    confirmacao = PasswordField('Confirmação da senha')
+    username = StringField('Nome de usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao = PasswordField('Confirmação da senha', validators=[DataRequired(), EqualTo('senha')])
     botao_submit_criarconta = SubmitField('Criar conta')
 
 
 class FormLogin(FlaskForm):
-    email = StringField('E-mail')
-    senha = PasswordField('Senha')
-    botao_submit_criarconta = SubmitField('Fazer Login')
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    botao_submit_login = SubmitField('Fazer Login')
