77ebff60d4ae6cbb1da4b89929c4496c1f437fe1
lucascomaru <lucascomaru20@gmail.com>
2022-11-03 11:17:08 -0300

    Página de login personalizada com bootstrap.

--- main.py
+++ main.py
@@ -7,23 +7,27 @@ lista_usuarios = ['Lucas', 'Gabriel', 'Fátima', 'Vinicius', 'Cláudia']
 
 app.config['SECRET_KEY'] = 'e2f8274d48dc01d27c9ab6b844109580'
 
-@app.route("/")
+@app.route('/')
 def home():
     return render_template('home.html')
 
-@app.route("/contato")
+
+@app.route('/contato')
 def contato():
     return render_template('contato.html')
 
-@app.route("/usuarios")
+
+@app.route('/usuarios')
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
-@app.route("/login")
+
+@app.route('/login')
 def login():
     form_login = FormLogin()
-    form_criar_conta = FormCriarConta()
-    return render_template('login.html', form_login=form_login, form_criar_conta=form_criar_conta)
+    form_criarconta = FormCriarConta()
+    return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
