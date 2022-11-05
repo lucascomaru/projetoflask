efe506fa31b52f0b32f9d02c1619ba345d8c08b9
lucascomaru <lucascomaru20@gmail.com>
2022-11-05 14:08:47 -0300

    Atualizando models

--- models.py
+++ models.py
@@ -8,11 +8,14 @@ class Usuario(database.Model):
     email = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
     foto_perfil = database.Column(database.String, defaut='defaut.jpg')
+    posts = database.relationship('Post', backref='autor', lazy=True)
+    cursos = database.Column(database.String, nullable=False, Default='Não informado')
 
 class Post(database.Model):
     id = database.Column(database.Integer, primary_key=True)
     titulo = database.Column(database.String, nullable=False)
     corpo = database.Column(database.Text, nullable=False)
     data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow())
+    id_usuario = database.Column(database.Integer,database.ForeignKey('usuario.id'), nullable=False)
 
 
