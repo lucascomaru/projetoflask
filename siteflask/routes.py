a39a9a733f5126ff326e8213e914d259ccfea953
lucascomaru <lucascomaru20@gmail.com>
2022-11-07 16:03:34 -0300

    Personalizando login dos usuários

--- siteflask/routes.py
+++ siteflask/routes.py
@@ -1,8 +1,11 @@
 from flask import render_template, redirect, url_for, flash, request
-from siteflask import app
+from siteflask import app, database, bcrypt
 from siteflask.forms import FormLogin, FormCriarConta
+from siteflask.models import Usuario
+from flask_login import login_user
 
 lista_usuarios = ['Lucas', 'Gabriel', 'Fátima', 'Vinicius', 'Cláudia'] #variavel de teste
+
 
 @app.route('/')
 def home():
@@ -24,9 +27,19 @@ def login():
     form_login = FormLogin()
     form_criarconta = FormCriarConta()
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form:
-        flash(f'Login feito com sucesso no e-mail {form_login.email.data},', 'alert-success')
-        return redirect(url_for('home'))
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_dados.data)
+            flash(f'Login feito com sucesso no e-mail {form_login.email.data},', 'alert-success')
+            return redirect(url_for('home'))
+        else:
+            flash(f'Falha no login, e-mail ou senha incorretos' , 'alert-danger')
     if form_criarconta.validate_on_submit() and 'botao_submit_criarconta' in request.form:
+        senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
+        usuario = Usuario(username=form_criarconta.username.data, email=form_criarconta.email.data,
+                          senha=senha_cript)
+        database.session.add(usuario)
+        database.session.commit()
         flash(f'Conta criada para o e-mail {form_criarconta.email.data},', 'alert-success')
         return redirect(url_for('home'))
     return render_template('login.html', form_login=form_login, form_criarconta=form_criarconta)
