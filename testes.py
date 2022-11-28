c0e00013da44cd8dd2f4e48b613f9f34bbeb5928
Lucas Comaru <lucascomaru20@gmail.com>
2022-11-28 09:45:00 -0300

    segundo commit

--- testes.py
+++
@@ -1,9 +0,0 @@
-from siteflask import app, database
-from siteflask.models import Usuario, Post
-
-#with app.app_context():
-#    database.create_all()
-
-with app.app_context():
-    database.create_all()
-
